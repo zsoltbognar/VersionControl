3eb7005e06a7bb8e917fdcce38910808405c03ea
Zsolt Bognár <zsolt.bognar@stud.uni-corvinus.hu>
2021-10-08 18:31:06 +0200

    Load data

--- ExcelExport/ExcelExport/Form1.cs
+++ ExcelExport/ExcelExport/Form1.cs
@@ -12,9 +12,19 @@ namespace ExcelExport
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> lakasok;
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+            dataGridView1.DataSource = lakasok;
+        }
+
+        public void LoadData()
+        {
+            lakasok = context.Flats.ToList();
+
         }
     }
 }
