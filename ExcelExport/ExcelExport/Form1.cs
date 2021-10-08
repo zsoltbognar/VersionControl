388c51c72a4d82ef03385bdfb6601e4ad9a26b4b
Zsolt Bognár <zsolt.bognar@stud.uni-corvinus.hu>
2021-10-08 18:40:31 +0200

    Add Excel namespace

--- ExcelExport/ExcelExport/Form1.cs
+++ ExcelExport/ExcelExport/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace ExcelExport
 {
@@ -19,6 +21,8 @@ namespace ExcelExport
             InitializeComponent();
             LoadData();
             dataGridView1.DataSource = lakasok;
+
+            
         }
 
         public void LoadData()
