117e9920c6f2e5fea04fc5b0c587c6fe596024d9
Zsolt Bognár <zsolt.bognar@stud.uni-corvinus.hu>
2021-10-22 10:23:35 +0200

    Datagridview connection

--- VaR/VaR/Form1.cs
+++ VaR/VaR/Form1.cs
@@ -12,9 +12,14 @@ namespace VaR
 {
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
+
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
         }
     }
 }
