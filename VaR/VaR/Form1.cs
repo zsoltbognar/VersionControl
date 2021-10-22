197d2b36c14ab723b772015db7b42ab620f8f582
Zsolt Bognár <zsolt.bognar@stud.uni-corvinus.hu>
2021-10-22 13:46:28 +0200

    Add GetPortfolioValue

--- VaR/VaR/Form1.cs
+++ VaR/VaR/Form1.cs
@@ -35,5 +35,20 @@ namespace VaR
             Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
             dataGridView2.DataSource = Portfolio;
         }
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+            foreach (var item in Portfolio)
+            {
+                var last = (from x in Ticks
+                            where item.Index == x.Index.Trim()
+                               && date <= x.TradingDay
+                            orderby x.TradingDay
+                            select x)
+                            .First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
+        }
     }
 }
