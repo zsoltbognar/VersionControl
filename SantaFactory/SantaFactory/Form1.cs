f93c3477fd41f551f96e8597c4c373ab2bb87cad
Zsolt Bognár <zsolt.bognar@stud.uni-corvinus.hu>
2021-11-12 16:18:01 +0100

    Labda megjelenítés osztályon keresztül

--- SantaFactory/SantaFactory/Form1.cs
+++ SantaFactory/SantaFactory/Form1.cs
@@ -13,10 +13,22 @@ namespace SantaFactory
 {
     public partial class Form1 : Form
     {
+        List<Ball> _balls = new List<Ball>();
+        private BallFactory _ballFactory;
+
+        public BallFactory BallFactory
+        {
+            get { return _ballFactory; }
+            set { _ballFactory = value; }
+        }
+
         public Form1()
         {
             InitializeComponent();
-          
+            BallFactory = new BallFactory();
+
+            var b = BallFactory.CreateNew();
+            Controls.Add(b);
         }
     }
 }
